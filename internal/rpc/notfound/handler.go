80aaa341c7da426d0ca59042029c02a453a667b2
Oleksandr Shepetko <a@shepetko.com>
2024-12-28 23:25:48 +0200

    Fix logging

--- internal/rpc/notfound/handler.go
+++ internal/rpc/notfound/handler.go
@@ -3,6 +3,7 @@ package notfound
 import (
 	"net/http"
 
+	"github.com/ashep/d5y/internal/rpc/rpcutil"
 	"github.com/rs/zerolog"
 )
 
@@ -16,7 +17,8 @@ func New(l zerolog.Logger) *Handler {
 	}
 }
 
-func (h *Handler) Handle(w http.ResponseWriter, r *http.Request) {
-	h.l.Warn().Str("path", r.URL.Path).Msg("not found")
-	w.WriteHeader(http.StatusNotFound)
+func (h *Handler) Handle(rw http.ResponseWriter, req *http.Request) {
+	l := rpcutil.ReqLog(req, h.l)
+	l.Warn().Str("path", req.URL.Path).Msg("not found")
+	rw.WriteHeader(http.StatusNotFound)
 }
